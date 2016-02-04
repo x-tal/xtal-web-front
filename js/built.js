71198953efc41f4772af74440da4346a634762c0
grammaright <grammaright@gmail.com>
2016-02-04 19:11:16 +0900

    Add basic of facebook plugin

--- js/built.js
+++ js/built.js
@@ -13,4 +13,20 @@ $(document).ready(function() {
     $('#three-button').click(function() {
       $('#three-block').slideToggle();
     });
-})
+});
+
+// window.fbAsyncInit = function() {
+//   FB.init({
+//     appId      : '1215941241752783',
+//     xfbml      : true,
+//     version    : 'v2.5'
+//   });
+// };
+//
+// (function(d, s, id){
+//    var js, fjs = d.getElementsByTagName(s)[0];
+//    if (d.getElementById(id)) {return;}
+//    js = d.createElement(s); js.id = id;
+//    js.src = "//connect.facebook.net/en_US/sdk.js";
+//    fjs.parentNode.insertBefore(js, fjs);
+//  }(document, 'script', 'facebook-jssdk'));
